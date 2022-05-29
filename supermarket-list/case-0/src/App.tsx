74111c497961715ec57b1d3005fadc5cb7ef76f9
Jonathan Buitrago <jonathanbuitragoroncancio50@gmail.com>
2022-05-28 19:47:32 -0500

    challenge solved!!

--- supermarket-list/case-0/src/App.tsx
+++ supermarket-list/case-0/src/App.tsx
@@ -1,28 +1,47 @@
 import type {Item} from "./types";
 
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 
 import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const inputValue = useRef('')
+  const [items, setItems] = useState<Item[] | null>([]);
+
+  const deleteItem = async (id: number) => {
+    const data = await api.delete(items, id)
+    setItems([...data])     
+  }
+
+  const getItems = () => {
+    api.list().then(i => setItems(i));
+  }
+
+  const addItem = () => {
+    setItems([...items, {
+      id: items?.length + 1,
+      text: inputValue.current.value,
+      completed: false,
+    }]);
+    inputValue.current.value = ''
+  }
 
   useEffect(() => {
-    api.list().then(setItems);
+    getItems()
   }, []);
 
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
-        <button>Add</button>
+        <input ref={inputValue} name="text" type="text"  autoFocus />
+        <button type="button" onClick={() => addItem()}>Add</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text} <button onClick={() => deleteItem(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
--- supermarket-list/case-0/src/api.ts
+++ supermarket-list/case-0/src/api.ts
@@ -1,26 +1,45 @@
 import {Item} from "./types";
 
+let listItems = [
+  {
+    id: 1,
+    text: "Some thing to buy",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Some other thing to buy",
+    completed: true,
+  },
+  {
+    id: 3,
+    text: "Some last to buy",
+    completed: false,
+  },
+]
+
 export default {
   list: (): Promise<Item[]> =>
     new Promise((resolve) =>
       setTimeout(() => {
-        resolve([
-          {
-            id: 1,
-            text: "Some thing to buy",
-            completed: false,
-          },
-          {
-            id: 2,
-            text: "Some other thing to buy",
-            completed: true,
-          },
-          {
-            id: 3,
-            text: "Some last to buy",
-            completed: false,
-          },
-        ]);
+        resolve(listItems);
       }, 1000),
     ),
+  delete: (items: Item[], id: number): Promise<Item[]> =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        let index = items.findIndex(i => i.id === id)
+        items[index].completed = !items[index].completed
+        resolve(items);
+      }, 0),
+    ),
+  create: (data: Item): Promise<Item[]> => 
+      new Promise((resolve) => 
+        setTimeout(() => {
+          console.log(data)
+          listItems.push(data)
+          resolve(listItems)
+          console.log(listItems)
+        }, 500)
+      )
 };
